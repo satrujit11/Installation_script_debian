1ceb6547acbb2e6b9068333fd94f7a24b11c7e7a
satrujit11 <beherasatrujit12@gmail.com>
2023-01-20 00:01:06 +0530

    third

--- install.sh
+++ install.sh
@@ -4,5 +4,3 @@
 apt update && apt upgrade -y            #update and Upgrade
 apt install nala -y                                 # install nala to make linux terminal organized
 nala update && nala upgrade -y         # update and upgrade using nala
-
-
