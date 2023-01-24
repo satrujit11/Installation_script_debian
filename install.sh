269201148dd31aaddd23f521c188c15a105db4dd
Satrujit <satrujit@pop-os.localdomain>
2023-01-25 00:33:22 +0530

    try

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@
 apt update && apt upgrade -y            #update and Upgrade
 apt install nala -y                                 # install nala to make linux terminal organized
 nala update && nala upgrade -y         # update and upgrade using nala
-#sdhddshdh
+#sd
