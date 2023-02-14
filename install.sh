4eee8404e18f5b85608a66496b60096eec3fe9bc
Satrujit Behera <66621875+satrujit11@users.noreply.github.com>
2023-02-15 00:57:44 +0530

    readme

--- install.sh
+++ install.sh
@@ -5,3 +5,5 @@ apt update && apt upgrade -y            #update and Upgrade
 apt install nala -y                                 # install nala to make linux terminal organized
 nala update && nala upgrade -y         # update and upgrade using nala
 #sd
+
+# thank you
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Personal Use Installation file
+
+It is a personal file
+
+* 1
+* 2
+<br>
+1. todo
+2. not todo
